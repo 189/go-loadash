783142228e2289b4d8ef8ba7540fe5b4cc4e626d
189 <189>
2024-10-30 12:42:06 +0800

    chore: add format duration

---
+++ times_example_test.go
@@ -0,0 +1,15 @@
+package golodash
+
+import (
+	"fmt"
+	"time"
+)
+
+func ExampleFormatDuration() {
+	d := time.Second*3 + time.Hour*2 + time.Minute*21
+	str := FormatDuration(d, "HH:mm:ss")
+
+	fmt.Println(str)
+	// Output:
+	// 02:21:03
+}
---
+++ times_test.go
@@ -0,0 +1,18 @@
+package golodash
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatDuration(t *testing.T) {
+	is := assert.New(t)
+
+	d := time.Second*3 + time.Hour*2 + time.Minute*21
+	str := FormatDuration(d, "HH:mm:ss")
+
+	expected := "02:21:03"
+	is.Equal(expected, str)
+}
